55384a74da3474a8ed43de59d7feb51f820164a1
Sarthak <sarthakprajapati29@gmail.com>
2026-06-04 11:34:02 +0530

    Program to update water tank level

---
+++ Classroom/insertion_4th_june/water_tank.py
@@ -0,0 +1,6 @@
+#Program to check if water tank is full or not
+water_level=0
+while (water_level<=100):
+    print("Water level:",water_level,"liters")
+    water_level = water_level + 10
+print("Water tank is full.")
