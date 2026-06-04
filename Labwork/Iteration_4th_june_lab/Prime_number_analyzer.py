0af8f8c701543c88722e79e516a8143454ced8f2
Sarthak <sarthakprajapati29@gmail.com>
2026-06-04 14:02:16 +0530

    Program to check the given number is prime or not , if not then finding its factors

---
+++ Labwork/Iteration_4th_june_lab/Prime_number_analyzer.py
@@ -0,0 +1,18 @@
+#Program to check if a number is prime or not
+#Taking input from the user
+num=int(input("Enter a number: "))
+if num>1:
+    i=2
+    while i<num:
+        if (num%i)==0:
+            #factors of num are found, so it is not a prime number
+            print("Factors of",num,"are:")
+            while i<num:
+                if (num%i)==0:
+                     print(i)
+                i+=1
+            print(num,"is not a prime number")
+            break
+        i+=1
+    else:
+        print(num,"is a prime number")
