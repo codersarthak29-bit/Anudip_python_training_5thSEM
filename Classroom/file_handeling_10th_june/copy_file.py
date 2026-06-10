14f5c0a06625e402858cbbe490d8bb1a3b4808b0
Sarthak <sarthakprajapati29@gmail.com>
2026-06-10 15:08:15 +0530

    Program to copy entire content from one file into another

---
+++ Classroom/file_handeling_10th_june/copy_file.py
@@ -0,0 +1,20 @@
+''' Write a program to copy entire content from one file into another'''
+# Program to copy entire content from one file into another
+
+# Open source file in read mode
+source = open("text.txt", "r")
+
+# Read all content
+data = source.read()
+
+# Open destination file in write mode
+destination = open("copy.txt", "w")
+
+# Write content into destination file
+destination.write(data)
+
+# Close both files
+source.close()
+destination.close()
+
+print("File copied successfully.")
